4a013c18144648fa593b44377f47afd96f9aa9cc
magsmenuser7 <magsmenuser7@gmail.com>
2025-11-17 18:11:11 +0530

    changes fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/backstagebrains/" : "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
